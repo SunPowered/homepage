90e3619549ec801be58f7973f49c6eeb810e996c
SunPowered <tim@vanboxtel.ca>
2012-10-24 14:01:53 -0400

    PHP process login

--- tim/php/db_connect.php
+++ tim/php/db_connect.php
@@ -5,6 +5,5 @@ define("USER", 'www-db');
 define("PASSWORD", 'Emcg5TWrguo_nSGm');
 define("DATABASE", 'sunpowered');
 
-$db_conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
-
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 ?>
--- tim/php/process_login.php
+++ tim/php/process_login.php
@@ -1,2 +1,20 @@
 <?php
+include db_connect.php;
+include common.php;
+
+secure_session_start();
+
+if (isset($_POST['email'], $_POST['p'])) {
+	$email = $_POST['email'];
+	$password = $_POST['p'];
+	
+	if (login($email, $password, $mysqli) == true) {
+		echo "Login Success";
+	} else {
+		echo "Login failed";
+	}
+	
+} else {
+	echo 'Invalid request';
+}
 ?>
